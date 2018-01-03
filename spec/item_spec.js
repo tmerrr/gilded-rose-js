cf8dcaca9d035f8eb98cadf503ed1fc2ee1b5d3f
Tom Moir <trmoir9@gmail.com>
2018-01-03 12:28:56 +0000

    added increase and decrease quality functions to Item
    now need to implement this into the Shop class

--- spec/item_spec.js
+++ spec/item_spec.js
@@ -1,17 +1,39 @@
 describe ('Item', () => {
   beforeEach( () => {
     item = new Item('cheese', 2, 5);
+    freeItem = new Item ('free!', 1, 0)
   });
 
-  it ('has an item name', () => {
-    expect(item.name).toEqual('cheese');
+  describe ('Properties:', () => {
+    it ('has an item name', () => {
+      expect(item.name).toEqual('cheese');
+    });
+
+    it ('has a Sell In value', () => {
+      expect(item.sellIn).toEqual(2);
+    });
+
+    it ('has a quality value', () => {
+      expect(item.quality).toEqual(5);
+    });
   });
 
-  it ('has a Sell In value', () => {
-    expect(item.sellIn).toEqual(2);
+  describe ('#increaseQuality', () => {
+    it ('increases quality by 1', () => {
+      item.increaseQuality();
+      expect(item.quality).toEqual(6);
+    });
   });
 
-  it ('has a quality value', () => {
-    expect(item.quality).toEqual(5);
+  describe ('#decreaseQuality', () => {
+    it ('decreases quality by 1', () => {
+      item.decreaseQuality();
+      expect(item.quality).toEqual(4);
+    });
+
+    it ('cannot reduce quality below 0', () => {
+      freeItem.decreaseQuality();
+      expect(freeItem.quality).toEqual(0);
+    })
   });
 });
--- src/item.js
+++ src/item.js
@@ -4,4 +4,12 @@ class Item {
     this.sellIn = sellIn;
     this.quality = quality;
   }
+
+  increaseQuality() {
+    this.quality++;
+  }
+
+  decreaseQuality() {
+    if (this.quality > 0) this.quality--;
+  }
 }
