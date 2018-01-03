7310acc28d5e607768319499cddbfa6b4ba9747e
Tom Moir <trmoir9@gmail.com>
2018-01-03 12:34:30 +0000

    the Shops updateQuality function now calles the increase and decrease Quality function on the item when needed
    this has replaced the Shop reducing the item quality by 1. Now need to do the same for sellIn property and also for setting the quality to 0 of expired products

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -7,21 +7,21 @@ class Shop {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
+            this.items[i].decreaseQuality();
           }
         }
       } else {
         if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
+          this.items[i].increaseQuality();
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+                this.items[i].increaseQuality();
               }
             }
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+                this.items[i].increaseQuality();
               }
             }
           }
@@ -35,7 +35,7 @@ class Shop {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
+                this.items[i].decreaseQuality();
               }
             }
           } else {
@@ -43,7 +43,7 @@ class Shop {
           }
         } else {
           if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+            this.items[i].increaseQuality();
           }
         }
       }
